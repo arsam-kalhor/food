1b35e420147d76e106608b5df6f0c17da525863b
Arsam-Kalhor <arsamkalhor52@gmail.com>
2025-10-30 22:51:40 +0330

    make the project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import NavBar from "./components/NavBar/NavBar.js";
+import { BrowserRouter , Route , Routes} from 'react-router-dom';
+import Home from './Pages/Home/Home.js';
+import Create from './Pages/Create/Create.js';
+import Recipe from './Pages/Recipe/Recipe.js';
+import Search from './Pages/Search/Search.js';
 
-function App() {
+
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <BrowserRouter>
+     <NavBar/>
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/Create" element={<Create/>}/>
+      <Route path="/Recipe/:id" element={<Recipe/>}/>
+      <Route path="/Search" element={<Search/>}/>
+    </Routes>
+    </BrowserRouter>
+  )
 }
-
-export default App;
