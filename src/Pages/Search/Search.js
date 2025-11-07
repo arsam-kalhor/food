ed676cb0831ccc102dc51502cb6dd811dceeef2d
Arsam-Kalhor <arsamkalhor52@gmail.com>
2025-11-07 11:48:26 +0330

    a little change in recipe  css

--- src/Pages/Search/Search.js
+++ src/Pages/Search/Search.js
@@ -1,15 +1,18 @@
 import { useLocation } from 'react-router-dom'
 import useFetch from '../../hooks/useFetch'
 import Cards from '../../components/Cards/Cards'
+import {useTheme} from "../../hooks/useTheme";
+import './Search.css'
 
 export default function Search() {
     const queryString = useLocation().search
     const queryParams = new URLSearchParams(queryString)
     const query = queryParams.get('q')
     const {data , error} = useFetch('https://food-api-dx0q.onrender.com/recipes?q=' + query)
+    const {mode} = useTheme()
   return (
-    <div>
-        <h2 className="page-title">Recipes including "{query}"</h2>
+    <div className={`search-container ${mode}`}>
+        <h2 className={`page-title ${mode}`}>Recipes including "{query}"</h2>
         {error && <p className='error'>{error}</p>}
         {data && <Cards recipes={data}/>}
     </div>
